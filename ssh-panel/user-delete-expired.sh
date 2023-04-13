692539d729caa1f521a80ec0b00d283bc6188942
MiladMk <miladm1995@yahoo.com>
2023-04-13 17:26:44 +0330

    Create user-delete-expired.sh

---
+++ ssh-panel/user-delete-expired.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+clear
+
+if [ ! -f /usr/local/bin/deleteduser ]; then
+    echo "echo "Script Created By VPS-Murah.net"" > /usr/local/bin/deleteduser
+	chmod +x /usr/local/bin/deleteduser
+fi
+hariini=`date +%d-%m-%Y`
+echo " "
+echo " "
+echo "-------------------------------------------"
+cat /etc/shadow | cut -d: -f1,8 | sed /:$/d > /tmp/expirelist.txt
+totalaccounts=`cat /tmp/expirelist.txt | wc -l`
+for((i=1; i<=$totalaccounts; i++ ))
+       do
+       tuserval=`head -n $i /tmp/expirelist.txt | tail -n 1`
+       username=`echo $tuserval | cut -f1 -d:`
+       userexp=`echo $tuserval | cut -f2 -d:`
+       userexpireinseconds=$(( $userexp * 86400 ))
+       tglexp=`date -d @$userexpireinseconds`             
+       tgl=`echo $tglexp |awk -F" " '{print $3}'`
+       while [ ${#tgl} -lt 2 ]
+       do
+           tgl="0"$tgl
+       done
+       while [ ${#username} -lt 15 ]
+       do
+           username=$username" " 
+       done
+       bulantahun=`echo $tglexp |awk -F" " '{print $2,$6}'`
+       echo "echo "VPS-Murah.net- User : $username Date Expired On : $tgl $bulantahun"" >> /usr/local/bin/alluser
+       todaystime=`date +%s`
+       if [ $userexpireinseconds -ge $todaystime ] ;
+           then
+			:
+       else
+       echo "echo "Username : $username already expired On Date: $tgl $bulantahun has been deleted on:: $hariini "" >> /usr/local/bin/deleteduser
+	   echo "Username $username expired on $tgl $bulantahun successfully removed from the system on $hariini"
+       userdel $username
+       fi
+done
+
+echo -e "\nPress Enter key to return to main menu"; read
+menu
