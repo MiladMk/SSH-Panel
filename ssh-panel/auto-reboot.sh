f5eb73fe00a26a9d6ef29ae9a2f77b54a7675c02
MiladMk <miladm1995@yahoo.com>
2023-04-13 20:02:39 +0330

    Update auto-reboot.sh

--- ssh-panel/auto-reboot.sh
+++ ssh-panel/auto-reboot.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 clear
 
-
-if [ ! -e /usr/local/bin/reboot_otomatis ]; then
-echo '#!/bin/bash' > /usr/local/bin/reboot_otomatis 
-echo 'tanggal=$(date +"%m-%d-%Y")' >> /usr/local/bin/reboot_otomatis 
-echo 'waktu=$(date +"%T")' >> /usr/local/bin/reboot_otomatis 
-echo 'echo "Server successfully rebooted on the date of $tanggal hit $waktu." >> /root/log-reboot.txt' >> /usr/local/bin/reboot_otomatis 
-echo '/sbin/shutdown -r now' >> /usr/local/bin/reboot_otomatis 
-chmod +x /usr/local/bin/reboot_otomatis
+if [ ! -e /etc/ssh-panel/reboot.sh ]; then
+echo '#!/bin/bash' > /etc/ssh-panel/reboot.sh 
+echo 'tanggal=$(date +"%m-%d-%Y")' >> /etc/ssh-panel/reboot.sh
+echo 'waktu=$(date +"%T")' >> /etc/ssh-panel/reboot.sh 
+echo 'echo "Server successfully rebooted on the date of $tanggal hit $waktu." >> /root/log-reboot.txt' >> /etc/ssh-panel/reboot.sh
+echo '/sbin/shutdown -r now' >> /etc/ssh-panel/reboo.sh
+chmod +x /etc/ssh-panel/reboot.sh
 fi
 
 echo "-------------------------------------------"
@@ -27,25 +26,25 @@ echo "-------------------------------------------"
 read -p "Select options from (1-9): " x
 
 if test $x -eq 1; then
-echo "10 * * * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "10 * * * * root /etc/ssh-panel/reboot.sh" > /etc/cron.d/reboot.sh
 echo "Auto-Reboot has been set every an hour."
 elif test $x -eq 2; then
-echo "10 */6 * * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "10 */6 * * * root /etc/ssh-panel/reboot.sh" > /etc/cron.d/reboot.sh
 echo "Auto-Reboot has been successfully set every 6 hours."
 elif test $x -eq 3; then
-echo "10 */12 * * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "10 */12 * * * root /etc/ssh-panel/reboot.sh" > /etc/cron.d/reboot.sh
 echo "Auto-Reboot has been successfully set every 12 hours."
 elif test $x -eq 4; then
-echo "10 4 * * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "10 4 * * * root /etc/ssh-panel/reboot.sh" > /etc/cron.d/reboot.sh
 echo "Auto-Reboot has been successfully set once a day."
 elif test $x -eq 5; then
-echo "10 0 */7 * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "10 0 */7 * * root /etc/ssh-panel/reboot.sh" > /etc/cron.d/reboot.sh
 echo "Auto-Reboot has been successfully set once a week."
 elif test $x -eq 6; then
-echo "10 0 1 * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "10 0 1 * * root /etc/ssh-panel/reboot.sh" > /etc/cron.d/reboot.sh
 echo "Auto-Reboot has been successfully set once a month."
 elif test $x -eq 7; then
-rm -f /etc/cron.d/reboot_otomatis
+rm -f /etc/cron.d/reboot.sh
 echo "Auto-Reboot successfully TURNED OFF."
 elif test $x -eq 8; then
 if [ ! -e /root/log-reboot.txt ]; then
