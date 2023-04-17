3e49b8ee11896c462d6afbc5d732c90c83067ecc
MiladMk <miladm1995@yahoo.com>
2023-04-17 11:24:19 +0330

    Add files via upload

---
+++ ssh-panel/backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+IP=$(wget -qO- ipinfo.io/ip);
+date=$(date +"%Y-%m-%d")
+clear
+echo "Create Backup"
+cd /root
+rm -rf /root/backup
+mkdir /root/backup
+cp /etc/passwd backup/
+cp /etc/group backup/
+cp /etc/shadow backup/
+cp /etc/gshadow backup/
+cd /root
+zip -r $IP-$date.zip backup > /dev/null 2>&1
+
+echo -e "
+Detail Backup 
+==================================
+IP VPS        : $IP
+Directory     : /root/$IP-$date.zip
+Date          : $date
+==================================
+"
+echo -e "\nPress Enter key to return to main menu"; read
+menu
---
+++ ssh-panel/restore.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# My Telegram : https://t.me/Akbar218
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+clear
+echo "This Feature Can Only Be Used According To Vps Data With This Autoscript"
+echo "Please input link to your vps data backup file."
+echo "You can check it on your email if you run backup data vps before."
+read -rp "Link File: " -e url
+wget -O backup.zip "$url"
+unzip backup.zip
+rm -f backup.zip
+sleep 1
+echo Start Restore
+cd /root/backup
+cp passwd /etc/
+cp group /etc/
+cp shadow /etc/
+cp gshadow /etc/
+
+echo Restore Done
+echo -e "\nPress Enter key to return to main menu"; read
+menu
