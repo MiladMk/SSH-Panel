70b0922f838816618aaf5dcff7e649d6ce0c5a90
MiladMk <miladm1995@yahoo.com>
2023-08-27 12:05:25 +0330

    Update unlock-blocked-users.sh

--- ssh-panel/unlock-blocked-users.sh
+++ ssh-panel/unlock-blocked-users.sh
@@ -5,4 +5,8 @@ users=$(<lock-list.txt)
 for i in $users
 do
   passwd -u $i
+  date=`date +"%Y-%m-%d %X"`;
+  echo "Unblock User $i - $date ";
+  echo "Unblock User $i - $date " >> /root/log-lock-limit.txt;
 done
+> lock-list.txt
