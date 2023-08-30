7e721ddfc5aa9cbc209ec9a498b97b95e2a9583a
MiladMk <miladm1995@yahoo.com>
2023-08-30 14:06:51 +0330

    Update ListUsers.sh

--- ssh-panel/ListUsers.sh
+++ ssh-panel/ListUsers.sh
@@ -10,41 +10,36 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
-# Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/senowahyu62/perizinan/main/ipvps.txt | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Facebook : https://m.facebook.com/lis.tio.718"
-echo -e "${NC}${LIGHT}WhatsApp : 081545854516"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/Akbar218"
-exit 0
-fi
 clear
-echo "---------------------------------------------------"
-echo "USERNAME          EXP DATE          STATUS"
-echo "---------------------------------------------------"
+echo "--------------------------------------------------------------------"
+echo "USERNAME          EXP DATE          STATUS            Days to left  "
+echo "--------------------------------------------------------------------"
 while read expired
 do
 AKUN="$(echo $expired | cut -d: -f1)"
 ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
 exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
 status="$(passwd -S $AKUN | awk '{print $2}' )"
-if [[ $ID -ge 1000 ]]; then
-if [[ "$status" = "L" ]]; then
-printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "LOCKED"
-else
-printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "UNLOCKED"
+if [[ "$exp" = "never" ]]; then
+  expires="Never"
+  else
+  expires=$(date +%s -d "$exp")
+  todaystime=`date +%s`
+  countday=$(((expires-todaystime)/86400))
 fi
+if [[ "$AKUN" != "udpgw" ]]; then
+  if [[ $ID -ge 1000 ]]; then
+    if [[ "$status" = "L" ]]; then
+      printf "%-17s %2s %-17s %01d \n" "$AKUN" "$exp     " "LOCKED        " "$countday"
+    else
+      printf "%-17s %2s %-17s %01d \n" "$AKUN" "$exp     " "UNLOCKED      " "$countday"
+    fi
+  fi
 fi
 done < /etc/passwd
 JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
 echo "---------------------------------------------------"
-echo "Account number: $JUMLAH user"
+echo "Account number: $(($JUMLAH-1)) user"
 echo "---------------------------------------------------"
 echo -e "\nPress Enter key to return to main menu"; read
 menu
